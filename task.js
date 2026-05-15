8bcb48aa0f60dcc7914cf37c6476501ecadb7d07
Azizli528 <mhrrm661@gmail.com>
2026-05-16 00:33:43 +0400

    dizayn demek olarki hazirdi ve js taski

---
+++ task.js
@@ -0,0 +1,5 @@
+let login = prompt("Logini daxil edin:");
+
+(login === "admin" && ((prompt("Sifreni daxil edin:") === "12345" && alert("Admin panele giriş edildi")) || 
+alert("Şifrə yanlisdir"))) || alert("Belə istifadəçi yoxdur");
+
